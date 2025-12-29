ffe2e95d64863a0b9ab333c05490f24a1a0224c5
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-12-29 12:19:17 +0100

    Update button text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            🔎 Browse Problem4J Documentation 🔎
+            📓 See Documentation 📓
           </Link>
         </div>
       </div>
